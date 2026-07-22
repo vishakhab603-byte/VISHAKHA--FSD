6e00e348c27b2291967e4c8cd108da986db58f75
VISHAKHA <vishakhab603@gmail.com>
2026-07-22 18:07:10 +0530

    Add files via upload

---
+++ EXPERIMENT 1.1.1/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PostComposer from "./components/PostComposer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dynamic Post Composer</h1>
+      <PostComposer />
+    </div>
+  );
+}
+
+export default App;
