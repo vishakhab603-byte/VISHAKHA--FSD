7d6a3c5cf75d816483e83737d2efcd1d1a208775
VISHAKHA <vishakhab603@gmail.com>
2026-07-29 20:42:29 +0530

    Delete EXPERIMENT 1.1.2 directory

--- EXPERIMENT 1.1.2/validation.js
+++
@@ -1,9 +0,0 @@
-export const platformLimits = {
-  Twitter: 280,
-  Facebook: 5000,
-  Instagram: 2200,
-  LinkedIn: 3000,
-};
-
-export const isValidPost = (platform, length) =>
-  length <= platformLimits[platform];
